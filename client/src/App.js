cca053ace3fb9ce2843f0b84e6f4842f7087324d
manash-barai <manashjbarai@gmail.com>
2024-10-06 23:51:21 +0530

    add client side with tailwind css

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-center py-50 text-9xl text-red-900'>
+      Hello Budy
+    </div>
+  )
+}
+
+export default App
